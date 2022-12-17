903ed5f1ea71132272af22ec0f698905126a5880
Delta <cebracho94@gmail.com>
2022-12-16 23:01:18 -0400

    fixing emptyFunc

--- src/components/Product/AddToCart.js
+++ src/components/Product/AddToCart.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { ShopButton } from 'styles/Product/Components/ShopButton'
 
 export const AddToCart = ({display, text ="Agregar al Carrito", redirectFunction = emptyFunc}) => {
+  const emptyFunc = () => {
+
+  }
+  
   if(display && display.includes("combo_list")) return <></>
 
   return (
